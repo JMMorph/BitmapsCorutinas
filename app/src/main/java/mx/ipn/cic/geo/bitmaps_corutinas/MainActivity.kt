ddfa1c9cc07207411a716dd111861daf5ff12470
JMMorph <jmedinap1600@alumno.ipn.mx>
2022-10-20 11:05:26 -0500

    Initial Commit

--- app/src/main/java/mx/ipn/cic/geo/bitmaps_corutinas/MainActivity.kt
+++ app/src/main/java/mx/ipn/cic/geo/bitmaps_corutinas/MainActivity.kt
@@ -1,11 +1,19 @@
 package mx.ipn.cic.geo.bitmaps_corutinas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import mx.ipn.cic.geo.bitmaps_corutinas.databinding.ActivityMainBinding
+import kotlin.math.max
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
    // Atributo para acceder a los widgets mediante vinculación de vista.
@@ -23,19 +31,79 @@ class MainActivity : AppCompatActivity() {
       binding = ActivityMainBinding.inflate(layoutInflater)
       val view = binding.root
       setContentView(view)
-
       // Ocultar el ActionBar.
       this.supportActionBar?.hide()
+
    }
 
    private fun initButtonListener() {
       // Recuperar la referencia al widget usando vinculación de vistas.
+      var imageViewBitmap = binding.imagen
+
+
       binding.btnCorutina.setOnClickListener {
          MainScope().launch {
-            binding.mensajeCorutina.text = resources.getText(R.string.mensaje_corutina_inicio)
-            delay(2000)
-            binding.mensajeCorutina.text = resources.getText(R.string.mensaje_corutina_fin)
+            //Toast.makeText(applicationContext, "Subrutina iniciada...", Toast.LENGTH_SHORT).show()
+            //delay(3000)
+            var nombre: String = "medieval" + Random.nextInt(0, 9).toString()
+            val id  = resources.getIdentifier(nombre, "drawable", packageName)
+
+            imageViewBitmap?.setImageBitmap(
+               decodeSampledBitmapFromResource(
+                  resources,
+                  id,
+                  imageViewBitmap!!.width,
+                  imageViewBitmap!!.height
+               )
+            )
+
+            //Toast.makeText(applicationContext, "Subrutina terminada :D...", Toast.LENGTH_SHORT).show()
          }
       }
    }
+
+   private fun calculateInSampleSize(options: BitmapFactory.Options, reqWidth: Int, reqHeight: Int): Int {
+      // Raw height and width of image
+      val height = options.outHeight
+      val width = options.outWidth
+      var inSampleSize = 1
+
+      // Tamaño original del mapa de bits (width 3300 x height 1650).
+      // Tamaño deseado (reqWidth 330 x reqHeight 200).
+      if (height > reqHeight || width > reqWidth) {
+
+         // Calculate ratios of height and width to requested height and width
+         // piso (floor) , techo (cell).
+         val heightRatio = (height.toFloat() / reqHeight.toFloat()).roundToInt() // Res: 8
+         val widthRatio = (width.toFloat() / reqWidth.toFloat()).roundToInt() // Res: 10.
+
+         // Choose the smallest ratio as inSampleSize value, this will guarantee
+         // a final image with both dimensions larger than or equal to the
+         // requested height and width.
+         inSampleSize = max(heightRatio, widthRatio) // 8
+      }
+      return inSampleSize
+   }
+
+   private fun decodeSampledBitmapFromResource(res: Resources?, resId: Int,
+                                               reqWidth: Int, reqHeight: Int): Bitmap?
+   {
+      // First decode with inJustDecodeBounds=true to check dimensions
+      val options = BitmapFactory.Options()
+      // De forma predeterminada el valor predeterminado es false. (default value is false).
+      options.inJustDecodeBounds = true
+      BitmapFactory.decodeResource(res, resId, options)
+
+      // Calculate inSampleSize
+      options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
+
+      // Decode bitmap with inSampleSize set
+      options.inJustDecodeBounds = false
+      return BitmapFactory.decodeResource(res, resId, options)
+   }
+
+
+
+
+
 }
